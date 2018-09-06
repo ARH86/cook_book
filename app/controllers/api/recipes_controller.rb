9e58026dd8e6380849de3b90800b28e303642325
Adam <ahanson845@gmail.com>
2018-09-05 21:31:19 -0500

    updated cookbook

---
+++ app/controllers/api/recipes_controller.rb
@@ -0,0 +1,56 @@
+class Api::RecipesController < ApplicationController
+  def index
+    @recipes = Recipe.all
+
+    search_term = params[:search]
+    if search_term 
+      @recipes = Recipe.where("titile iLIKE ?", "%#{"search_term"}%")
+    end
+    
+    sort_attribute = params[:sort]
+    if sort_attribute
+      @recipe = @recipe.order(sort_attribute)
+    end
+    render 'index.json.jbuilder'
+  end
+
+  def create
+    @recipe = Recipe.new(
+                         title: params[:title],
+                         chef: params[:chef],
+                         ingredients: params[:ingredients],
+                         directions: params[:directions],
+                         image_url: params[:image_url],
+                         prep_time: params[:prep_time]
+                        )
+    @recipe.save
+    render 'show.json.jbuilder'
+  end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+    render 'show.json.jbuilder'
+  end
+
+  def update
+    @recipe = Recipe.find(params[:id])
+
+    @recipe.title = params[:title] || @recipe.title
+    @recipe.chef = params[:chef] || @recipe.chef
+    @recipe.prep_time = params[:prep_time] || @recipe.prep_time
+    @recipe.ingredients = params[:ingredients] || @recipe.ingredients
+    @recipe.directions = params[:directions] || @recipe.directions
+    @recipe.image_url = params[:image_url] || @recipe.image_url
+
+    @recipe.save
+    render "show.json.jbuilder"
+  end
+
+  def destroy
+    @recipe = Recipe.find(params[:id])
+    @recipe.destroy
+    render json: {message: "Recipe successfully destroyed"}
+  end
+end
+
+
